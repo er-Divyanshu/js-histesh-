cb37c654f1f5cf0b3e7d8c00a3b714ed7b66eb12
er-Divyanshu <sharmadivyanshu126@gmail.com>
2026-06-07 18:12:09 +0000

    Add JavaScript basics files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId=144553
+let accountemail="divyanshu@gmail.com"
+var accountpassword="1234"
+console.log(accountId);
+console.table([accountId,accountemail,accountpassword])
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,9 @@
+let score="33"
+console.log(typeof score);
+let valueinnumber= Number(score);
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+// "33"=> 33
+//"33abc"=>NaN
+//true=>1,false=>0
+// 1=>true , 0=>false
