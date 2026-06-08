4f3de1962563560574793ad792c4e406787ac678
er-Divyanshu <sharmadivyanshu126@gmail.com>
2026-06-08 16:34:52 +0000

    Add JavaScript basics files

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,13 @@
+/// memory part 
+/// stack(primitive) and heap(non primitive)
+let myyoutubename="sharmadivyanshudotcom"
+let anothername=myyoutubename
+anothername=" web developer"
+// anothername will be chnaged but the myyoutubename value will remain same 
+let userone={
+    email:"user@gmail.com",
+    upi:"user@ptyes"
+}
+let usertwo=userone 
+usertwo.email="usertwo@gmail.com"
+console.log(userone.email);
---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+const gameName=new String('Hiteshhc')
+console.log(gameName.lenght)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf('h'))
+
